7165fe6523f6fe10215179f3be6fdcfbc6192e03
slimeyar <lostbhijaya@gmail.com>
2023-06-14 15:58:33 +0700

    Add more functionality to ChainItem

--- src/main/java/mod/yourname/yourmodid/chain/ChainItem.java
+++ src/main/java/mod/yourname/yourmodid/chain/ChainItem.java
@@ -1,7 +1,5 @@
 package mod.yourname.yourmodid.chain;
 
-import com.simibubi.create.content.contraptions.base.RotatedPillarKineticBlock;
-import com.simibubi.create.content.contraptions.relays.elementary.AbstractShaftBlock;
 import com.simibubi.create.content.contraptions.relays.elementary.CogWheelBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
@@ -11,10 +9,12 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 public class ChainItem extends Item {
     public ChainItem(Properties properties) {
@@ -29,27 +29,42 @@ public class ChainItem extends Item {
         Block block = level.getBlockState(clicked).getBlock();
 
         CompoundTag tag = pContext.getItemInHand().getOrCreateTag();
-        BlockPos first = null;
 
         if (!(block instanceof CogWheelBlock)) {
-            level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.AMETHYST_BLOCK_FALL, SoundSource.PLAYERS, 1.0F, 1.0F);
+            level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.AMETHYST_BLOCK_FALL, SoundSource.PLAYERS, 0.2F, 0.5F);
             return InteractionResult.FAIL;
         }
 
-        if (tag.contains("First")) {
-            first = NbtUtils.readBlockPos(tag.getCompound("First"));
+        if (tag.contains("1")) {
+            for (String key : tag.getAllKeys()) {
+                if (key.matches(".*\\d.*")) {
+                    BlockPos currSel = NbtUtils.readBlockPos(tag.getCompound(key));
 
-            if  (first.equals(clicked)) {
-                level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.CHAIN_BREAK, SoundSource.PLAYERS, 1.0F, 1.0F);
-                tag.remove("First");
-                return InteractionResult.FAIL;
+                    if (currSel.equals(clicked)) {
+                        player.getCooldowns().addCooldown(this, 20);
+                        level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.CHAIN_BREAK, SoundSource.PLAYERS, 1.0F, 1.0F);
+                        tag.remove("First");
+                        return InteractionResult.FAIL;
+                    }
+                }
             }
-            System.out.println(first.toString() + "Is Connected to" + clicked);
-            level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.CHAIN_PLACE, SoundSource.PLAYERS, 1.0F, 1.0F);
-            return InteractionResult.CONSUME;
+            if (player.isShiftKeyDown()) {
+                BlockPos currSel = NbtUtils.readBlockPos(tag.getCompound("1"));
+                System.out.println(currSel + "Is Connected to" + clicked);
+                player.getCooldowns().addCooldown(this, 10);
+                level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.CHAIN_PLACE, SoundSource.PLAYERS, 1.0F, 1.0F);
+                return InteractionResult.CONSUME;
+            }
+            Integer last = tag.getAllKeys().size();
+            tag.put((last + 1).toString(), NbtUtils.writeBlockPos(clicked))
+            pContext.getItemInHand().setTag(tag);
+            player.getCooldowns().addCooldown(this, 10);
+
+            level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.IRON_TRAPDOOR_OPEN, SoundSource.PLAYERS, 1.0F, 1.0F);
+            return InteractionResult.SUCCESS;
         }
 
-        tag.put("First", NbtUtils.writeBlockPos(clicked));
+        tag.put("1", NbtUtils.writeBlockPos(clicked));
         pContext.getItemInHand().setTag(tag);
         player.getCooldowns().addCooldown(this, 10);
 
